67e0cfb18559e4310d8d8d8b4d62691cf6fbf783
tariq <kapooky101@gmail.com>
2021-11-11 13:42:53 -0500

    main

--- removestacks.sh
+++ removestacks.sh
@@ -8,27 +8,31 @@
 set -euo pipefail
 
 #Insert the list of stack names that you want to retain.
-stacks_to_retain=('jobstack' '' 'eu-central-1a')
-
+stacks_to_retain=('jobstack' '' 'hello')
 
 
 for row in $(aws cloudformation list-stacks --stack-status-filter "CREATE_COMPLETE" | jq -r -c '.[][] | {StackName, StackId}'); do
-  name=$(echo ${row} | jq -r '.StackName')
+  stack_to_retain_NAME=$(echo ${row} | jq -r '.StackName')
   stackID=$(echo ${row} | jq -r '.stackId')
   isTemporaryStack=false
+
+  #for loop
   for name in "${stacks_to_retain[@]}"; do
-    set -x
-    if [ "$name" = "$name" ]; then
-      echo "The stack is important, preserve it."
+    if [ "$name" = "$stack_to_retain_NAME" ]; then
+      echo "${name} stack is important, preserve it."
+      #reset the flag to false
+      isTemporaryStack=false
       break
     else
-      echo "The stack is not neccesary, delete it. "
       isTemporaryStack=true
     fi
-    set +x
   done
+
+  #If statement to delete the CF template depending on if the isTemporaryStack flag is set.
   if [ "$isTemporaryStack" = true ]; then
     #Get the stackid and delete the CF stack.
-    cloudformation delete-stack --stack-name ${name}
+    aws cloudformation delete-stack --stack-name "${name}"
+    echo "${stack_to_retain_NAME} is being deleted."
   fi
+
 done
